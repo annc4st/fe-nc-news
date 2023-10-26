d9b45039b76365a7f175683038b7557529f87980
Ann <annaseniuta@gmail.com>
2023-10-26 15:04:45 +0100

    added everything into base root, deleted sub folder fe-vite-react

---
+++ src/SingleArticlePage.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getSingleArticle, getArticleComments, formatCommentDate, updateVotes} from "./api";
+
+import { Voter } from "./Voter";
+import Picker from 'emoji-picker-react';
+
+const SingleArticlePage = () => {
+  const { article_id } = useParams();
+  const [comments, setComments] = useState([]);
+  const [singleArticle, setSingleArticle] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getSingleArticle(article_id)
+      .then((article) => {
+        setIsLoading(false)
+        setSingleArticle(article);
+      })
+      .catch((error) => {
+      });
+    getArticleComments(article_id)
+      .then((comments) => {
+        setComments(comments);
+      })
+      .catch((error) => {
+      });
+  }, [article_id]);
+
+  if (isLoading) return <p className="loading-p">loading...</p>
+
+  return (
+    <section className="single-article">
+      {singleArticle ? ( 
+        <>
+      <div className="single-article-title">
+        <h2>{singleArticle.title}</h2>
+      </div>
+      <Voter votes = {singleArticle.votes}  article_id = {singleArticle.article_id}
+      setSingleArticle={setSingleArticle}/>
+
+
+      <div className="single-article-img">
+        <img src={singleArticle.article_img_url} alt="picture of" />
+      </div>
+
+      <div className="single-article-content">
+        <p> By {singleArticle.author}</p>
+        <p>topic {singleArticle.topic}</p>
+        <div className="article-body">{singleArticle.body}</div>
+      </div>
+      </> 
+      ) : (
+        <p> Loading ... </p>
+
+      )}
+
+      {/* comments */}
+
+      <div className="comments-section">
+        <h3>Comments</h3>
+
+        {comments.map((comment) => (
+          <div className="comment-div" key={comment.comment_id}>
+            <p>{comment.author}</p>
+            <p>{comment.body}</p>
+            <p>published: {formatCommentDate(comment.created_at)}</p>
+            <p>votes: {comment.votes}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default SingleArticlePage;
