2c773cae445a8e7e32d5301501bc26f66a100e30
Ann <annaseniuta@gmail.com>
2023-11-02 12:02:09 +0000

    added error page, home page image, broken heart emoji for negative votes for comments, postioned login button in one line with greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState} from 'react'
+import React, { useState} from 'react'
 import {Route, Routes, Link} from "react-router-dom";
 import NewsList from './NewsList';
 import SingleArticlePage from './SingleArticlePage';
+import Home from './Home';
 import Nav from './Nav';
 import Users from './Users';
 import Headline from './Headline';
 import SingleUser from './SingleUser';
 import {UserProvider} from './contexts/UserContext';
+import NotFound from './NotFound';
  
 
 const Header = () => {
   return (
  
     <div className='header'>
-  <h1 className='title'>Latest NC News </h1>
+      <img className = "header-img"
+      src="https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww"
+      alt="picture of some code on the dark scren" />
+      <div class="centered-header"><h1 className='title'>NC News </h1></div>
   </div>
   )
 };
@@ -23,7 +28,6 @@ function App() {
   const [user, setUser] = useState(null)
 
   return (
-   
       <>
       <UserProvider >
         <Header /> 
@@ -31,13 +35,14 @@ function App() {
         <Nav />   
 
         <Routes>
-        <Route path = "/" element = {<NewsList />} />
+        <Route path = "/" element = {<Home />} />
         <Route path = "/articles" element = {<NewsList />} />
         <Route path = "/articles/topics/:topic" element = {<NewsList />} />
         <Route path = "/articles/article/:article_id" element = {<SingleArticlePage/>}  />
         <Route path = "/users" element = {<Users />} />
         <Route path = "/users/:username" element = {<SingleUser />} />
-         
+        <Route path = "*" element = {<NotFound />} />
+
         </Routes>
         </UserProvider>
       </>
