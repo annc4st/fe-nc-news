9e4d5d7b38ec6dc196a826b180b79ddadfc1b944
annc4st <annaseniuta@gmail.com>
2023-12-07 20:10:41 +0000

    changed headline pic

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,8 @@ export default function Header() {
    
       <div className='header'>
         <img className = "header-img"
-        src="https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww"
+        src="src/assets/florian-olivo-4hbJ-eymZ1o.jpg"
+        // src="https://images.unsplash.com/photo-1555949963-ff9fe0c870eb?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww"
         alt="picture of some code on the dark scren" />
         <div class="centered-header"><h1 className='title'>NC News </h1></div>
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,9 +6,7 @@ const Home = ()  => {
         
         <div className='homePage-container'>
             <img className="homePage-image" 
-            src ="https://images.unsplash.com/photo-1627634777217-c864268db30c?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww"
-            
-        // src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&q=80&w=1740&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            src="src/assets/business-office_2_720.jpg"
         alt="home picture of people working on their computer creating news sitting at the table" />
         <div class="centered"><h2> Welcome To The NorthCoders News!</h2></div>
         
